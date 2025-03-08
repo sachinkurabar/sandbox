86c528660cb53c0da6100c54dfcf8379219b2356
sachin <sachinkurabar9@gmail.com>
2025-03-08 20:43:42 +0530

    commitng from vs code

---
+++ app.js
@@ -0,0 +1,80 @@
+//  alert("Hello World");
+//  console.log(123);
+//  console.log("Hello");
+//  console.log(true);
+//  var greeting = "hello";
+//  console.log(greeting);
+// console.log([1,2,3,4]);
+// console.log({a: 1, b: 2, c: 3});
+
+// var name = "Mani";
+// // console.log(name);
+// name = "Gourav";
+// console.log(name);
+
+// var greeting;
+// console.log(greeting);
+// greeting = "hello";
+// console.log(greeting);
+
+// let firstName = "Rakesh";
+// console.log(firstName);
+
+// const name = "Mani";
+// console.log(name);
+// name = "Gourav";
+//  console.log(name);
+
+// const person = {
+//     name : "Rakesh"
+// }
+// console.log(person);
+// person.name = "Deeksha";
+// console.log(person);
+
+// const number = [1,2,3,4,5];
+// number.push(6);
+// console.log(number);
+
+
+
+
+
+
+/*Two types of DataTypes in Javascript
+// Primitive DataTypes
+
+String
+Number
+Boolean
+null
+undefined
+Symbol
+
+// Referrenced DataType
+
+Object Literals
+Array
+Function
+*/
+
+
+// string
+const name = "Manikanta";
+
+// number
+const age = 18;
+
+// boolean
+const hasSiblings = true;
+
+// null
+const abc = null;
+
+// undefined
+let a;
+
+// symbol
+let sym = Symbol();
+
+console.log(typeof sym);
