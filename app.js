a453ea5942b8f9ce9417e10a9d24d2ca6bd593fe
sachin <sachinkurabar9@gmail.com>
2025-03-12 12:30:56 +0530

    Math Objects

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert("hello");
+// number and math object
+
+const num1 = 100;
+const num2 = 50;
+
+let val;
+ val = num1 + num2;
+ val = num1 - num2;
+ val = num1 * num2;
+ val = num1 / num2;
+ val = num1 % num2;
+
+// math object
+val = Math.PI;
+val = Math.round(2.5);
+val = Math.ceil(2.4);
+val = Math.floor(3.9);
+val = Math.sqrt(49);
+val = Math.abs(-8);
+val = Math.pow(2,4);
+val = Math.min(12,45,87,10,9);
+val = Math.max(12,45,87,10,9);
+val = Math.random() * 20 + 1;
+
+console.log(val);
